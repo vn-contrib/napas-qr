f51e5ba8ad85c77260fb0c33a1c646fce2e53055
tdd28 <tdd2810@gmail.com>
2026-01-10 00:17:48 +0700

    fix: responsive

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
         <code>npm install @vn-contrib/napas-qr</code> to use as a library or <br /> <code>{`<iframe src="${iframeUrl}?{querystring}" />`}</code> to embed in your website
       </p>
       <div className="card">
-        <p>
+        <p className="read-example">
           Example
           <br />
           <a href={expIframe} target="_blank">{expIframe}</a>
